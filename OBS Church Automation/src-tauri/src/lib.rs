0eea51415c8d632c4abb3f33b9cde593ebcbe0c9
Tiger <theorangetiger975@gmail.com>
2026-04-12 18:32:20 -0600

    modified:   OBS Church Automation/.gitignore 	modified:   OBS Church Automation/src-tauri/Cargo.lock 	modified:   OBS Church Automation/src-tauri/Cargo.toml 	new file:   OBS Church Automation/src-tauri/config.toml 	modified:   OBS Church Automation/src-tauri/src/lib.rs 	modified:   OBS Church Automation/src/index.html

--- OBS Church Automation/src-tauri/src/lib.rs
+++ OBS Church Automation/src-tauri/src/lib.rs
@@ -73,6 +73,7 @@ struct Config {
 }
 #[tauri::command]
 fn get_config() -> Config {
+
     let config: Config = match std::fs::read_to_string("config.toml") {
         Ok(s) => toml::from_str::<Config>(&s).unwrap_or(Config { cases: vec![] }),
         Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
@@ -81,7 +82,15 @@ fn get_config() -> Config {
         }
         Err(e) => panic!("Failed to read config: {}", e),
     };
-    config
+    return config;
+}
+async fn fetch_config() -> Config {
+    reqwest::get("https://example.com/file.txt")
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap()
 }
 #[tauri::command]
 fn init_main(name: &str) -> Main {
