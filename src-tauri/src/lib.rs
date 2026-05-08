5e45d1ea0f3604b4dca0935b6f6d37b723ceb588
TheOrangeTiger <theorangetiger975@gmail.com>
2026-05-08 17:15:19 -0600

    modified:   src-tauri/config.toml 	modified:   src-tauri/src/lib.rs 	modified:   src/index.html

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -76,12 +76,12 @@ fn get_config() -> Config {
     let config: Config = match std::fs::read_to_string("config.toml") {
         Ok(s) => toml::from_str::<Config>(&s).unwrap_or(Config { cases: vec![] }),
         Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
-            fs::write("config.toml", "# NUMBER MEANINGS\n# 0 = unidentified\n# 1 = credits\n# 2 = regular text\n# 3 = hymn\n# 4 = P: C:\n# 5 = insert empty scene\n# 6 = service name\n# 7 = lords prayer\n# 8 = special music\n# FORMAT\n# case = number\n[cases]").unwrap();
+            fs::write("config.toml", "# NUMBER MEANINGS\n# 0 = unidentified\n# 1 = credits\n# 2 = regular text\n# 3 = hymn\n# 4 = P: C:\n# 5 = insert empty scene\n# 6 = service name\n# 7 = N/A\n# 8 = special music\ncases = [\n#example[2, \"hello\"],\n]").unwrap();
             Config { cases: vec![] }
         }
         Err(e) => panic!("Failed to read config: {}", e),
     };
-    return config;
+    config
 }
 #[tauri::command]
 fn init_main(name: &str) -> Main {
@@ -120,7 +120,7 @@ fn add_textobj(mut main: Main, name: &str, scene: &str, contents: &str, fontsize
 fn bulletin_reader(app: tauri::AppHandle) -> Vec<String> {
     let mut lines = vec![];
     let file_path = app.dialog().file().blocking_pick_file().map(|x| x.to_string());
-    if file_path.is_none() { return vec!["ERROR".to_string()]; }
+    if file_path.is_none() { vec!["ERROR".to_string()] }
     else { 
         let file_path = file_path.unwrap();
         let f = File::open(file_path).expect("Failed to open file");
@@ -137,22 +137,101 @@ fn bulletin_categorizer(bulliten: Vec<String>, config: Config) -> Vec<(u32, Stri
     map.push((6, bulliten[0].clone()));
     while bulliten_index < bulliten.len() {
         let line = bulliten[bulliten_index].trim().to_string();
-        if line != "" {
+        if line.is_empty() {}
+        else if line.contains("Lord’s Prayer") {
+            map.push((2, line));
+            map.push((9, "Our Father, who art in heaven,
+                hallowed be thy Name,
+                thy kingdom come,
+                thy will be done,
+                on earth as it is in heaven.
+                Give us this day our daily bread.
+                And forgive us our trespasses,
+                as we forgive those
+                who trespass against us.
+                And lead us not into temptation,
+                but deliver us from evil.
+                For thine is the kingdom,
+                and the power, and the glory,
+                for ever and ever.
+                Amen.".to_string()));
+        }
+        else if line.contains("Apostles’ Creed") {
+            map.push((2, line));
+            map.push((9, "I believe in God,
+                the Father almighty,
+                Creator of heaven and earth,
+                and in Jesus Christ, his only Son, our Lord,
+                who was conceived by the Holy Spirit,
+                born of the Virgin Mary,
+                suffered under Pontius Pilate,
+                was crucified, died and was buried;
+                he descended into hell;
+                on the third day he rose again from the dead;
+                he ascended into heaven,
+                and is seated at the right hand of God the Father almighty;
+                from there he will come to judge the living and the dead.
+                I believe in the Holy Spirit,
+                the holy catholic Church,
+                the communion of saints,
+                the forgiveness of sins,
+                the resurrection of the body,
+                and life everlasting.
+                Amen.".to_string()));
+        }
+        else if line.contains("Nicene Creed") {
+            map.push((2, line));
+            map.push((9, "We believe in one God,
+                the Father almighty,
+                maker of heaven and earth,
+                of all things visible and invisible.
+                And in one Lord Jesus Christ,
+                the only Son of God,
+                begotten from the Father before all ages,
+                God from God,
+                Light from Light,
+                true God from true God,
+                begotten, not made;
+                of the same essence as the Father.
+                Through him all things were made.
+                For us and for our salvation
+                he came down from heaven;
+                he became incarnate by the Holy Spirit and the virgin Mary,
+                and was made human.
+                He was crucified for us under Pontius Pilate;
+                he suffered and was buried.
+                The third day he rose again, according to the Scriptures.
+                He ascended to heaven
+                and is seated at the right hand of the Father.
+                He will come again with glory
+                to judge the living and the dead.
+                His kingdom will never end.
+                And we believe in the Holy Spirit,
+                the Lord, the giver of life.
+                He proceeds from the Father and the Son,
+                and with the Father and the Son is worshiped and glorified.
+                He spoke through the prophets.
+                We believe in one holy catholic and apostolic church.
+                We affirm one baptism for the forgiveness of sins.
+                We look forward to the resurrection of the dead,
+                and to life in the world to come. Amen.".to_string()));
+        }
+        else {
             map.push((0, line.clone()));
             for case in &cases {
                 if line.contains(&case.1) {
                     if case.0 == 10  {
                         if line.starts_with(&case.1) {
                             map.pop();
-                            map.push((2, line.clone()));
+                            map.push((2, line));
                             bulliten_index += 1;
                             map.push((9, bulliten[bulliten_index].trim().to_string()));
                         }
                     } else {
                         map.pop();
-                        map.push((case.0, line.clone()));
+                        map.push((case.0, line));
                     }
-
+                    break
                 }
             }
         }   
@@ -170,9 +249,9 @@ fn build_livestream(map: Vec<(u32, String)>) -> Main {
         else if map[index].0 == 3 { main = add_scene(main, &format!("scn_{}", map[index].1)); }
         else if map[index].0 == 5 { if index + 1 >= map.len() || map[index + 1].0 != 5 { main = add_scene(main, &format!("scn_{}", map[index].1)); } }
         else if map[index].0 == 4 || map[index].0 == 1 || map[index].0 == 8 { main = add_scene(main, &format!("scn_{}", map[index].1)); main = add_textobj(main, &format!("txt_{}", map[index].1), &format!("scn_{}", map[index].1), &format!(" {} ", map[index].1), 50, Position {x: 0.0, y: 0.0}, 4278190080, 4294967295, 75, "center"); }
-        else if map[index].0 == 7 { main = add_scene(main, &format!("scn_{}", map[index].1)); main = add_textobj(main, &format!("txt_{}", map[index].1), &format!("scn_{}", map[index].1), " Our Father, who art in heaven,\n hallowed be thy Name,\n thy kingdom come,\n thy will be done,\n on earth as it is in heaven.\n Give us this day our daily bread.\n And forgive us our trespasses,\n as we forgive those who trespass against us.\n And lead us not into temptation,\n but deliver us from evil.\n For thine is the kingdom, and the power, and the glory, \n for ever and ever. Amen.", 50, Position {x: 20.0, y: 20.0}, 4278190080, 4294967295, 75, "left"); }
+        // else if map[index].0 == 7 { main = add_scene(main, &format!("scn_{}", map[index].1)); main = add_textobj(main, &format!("txt_{}", map[index].1), &format!("scn_{}", map[index].1), " Our Father, who art in heaven,\n hallowed be thy Name,\n thy kingdom come,\n thy will be done,\n on earth as it is in heaven.\n Give us this day our daily bread.\n And forgive us our trespasses,\n as we forgive those who trespass against us.\n And lead us not into temptation,\n but deliver us from evil.\n For thine is the kingdom, and the power, and the glory, \n for ever and ever. Amen.", 50, Position {x: 20.0, y: 20.0}, 4278190080, 4294967295, 75, "left"); }
         else if map[index].0 == 9 {
-            let mut temp_index = index.clone() - 1;
+            let mut temp_index = index - 1;
             loop {
                 if map[temp_index].0 == 1 || map[temp_index].0 == 2 || map[temp_index].0 == 4 {
                     if let Some(Source::Text { settings, .. }) = main.sources.iter_mut().find(|x| {
